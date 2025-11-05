e480c9a42f863bf30c528e8970e5269aaddc802f
Ting-Gian LUA <banacorn@gmail.com>
2025-11-05 19:45:56 +0800

    [ fix ] Type each index as `Index 200` to remove the GHC warnings

--- src/ChiF200.hs
+++ src/ChiF200.hs
@@ -5,45 +5,41 @@ module ChiF200
   , topEntity
   ) where
 
-import Clash.Prelude hiding (toList, map)
+import Clash.Prelude
 import Language.Haskell.TH
-import Data.Foldable (toList)
 import SHA3internal (State, sha3_constants, chi_constants)
 
 -- Chi transformation for b=200: l=3, w=8
--- Uses Template Haskell to generate explicit code with literal indices
+-- Uses lightweight TH to generate explicit code with literal indices
 
 type State200 = State 200
 
 -- | Chi transformation - generated via TH with literal indices
 chiF200 :: State200 -> State200
 chiF200 = $(do
-  -- Compute constants directly in TH splice to avoid stage restriction
-  let constants = sha3_constants @3 @8 @200
-      triples = toList (chi_constants constants)
-  sName <- newName "s"
+  let triples = toList (chi_constants (sha3_constants @3 @8 @200))
+  s <- newName "s"
 
-  let mkBitExpr :: Vec 3 (Index 200) -> Q Language.Haskell.TH.Exp
-      mkBitExpr (i0 :> i1 :> i2 :> Nil) = do
-        let idxE i = litE (integerL (toInteger i))
-            indexE i = infixE (Just (varE sName)) (varE '(!!)) (Just (idxE i))
-            compIndex i = appE (varE 'complement) (indexE i)
-        infixE (Just (indexE i0))
+  -- Type-annotated index literal to avoid defaulting warnings
+  let idx i = sigE (litE (integerL (toInteger i)))
+                   (conT ''Index `appT` litT (numTyLit 200))
+
+  -- Generate one bit expression: s !! i0 `xor` (complement (s !! i1) .&. s !! i2)
+  let bitExpr (i0 :> i1 :> i2 :> Nil) =
+        infixE (Just (infixE (Just (varE s)) (varE '(!!)) (Just (idx i0))))
                (varE 'xor)
-               (Just $ infixE (Just (compIndex i1))
-                              (varE '(.&.))
-                              (Just (indexE i2)))
-      mkBitExpr _ = fail "chi_constants entry must have length 3"
+               (Just (infixE (Just (appE (varE 'complement)
+                                         (infixE (Just (varE s)) (varE '(!!)) (Just (idx i1)))))
+                             (varE '(.&.))
+                             (Just (infixE (Just (varE s)) (varE '(!!)) (Just (idx i2))))))
+      bitExpr _ = fail "Expected 3 indices"
 
-  bitExprs <- mapM mkBitExpr triples
+  -- Build Vec from expressions
+  let vec [] = [| Nil |]
+      vec (e:es) = [| $(return e) :> $(vec es) |]
 
-  let buildVec :: [Language.Haskell.TH.Exp] -> Q Language.Haskell.TH.Exp
-      buildVec [] = [| Nil |]
-      buildVec (e:es) = [| $(return e) :> $(buildVec es) |]
-
-  vecExp <- buildVec bitExprs
-
-  lamE [varP sName] (return vecExp)
+  bitExprs <- mapM bitExpr triples
+  lamE [varP s] (vec bitExprs)
   )
 
 -- Hardware top entity - Chi transformation on 200-bit state
