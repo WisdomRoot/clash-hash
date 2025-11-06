2f2083fbbc94d87d4980ec5d911a73036e524bda
Ting-Gian LUA <banacorn@gmail.com>
2025-11-06 15:45:34 +0800

    [ change ] Add `iota` to `KeccakF200BitVec`

--- src/KeccakF200BitVec.hs
+++ src/KeccakF200BitVec.hs
@@ -1,12 +1,13 @@
 module KeccakF200BitVec
   ( chiF200BitVec
   , piF200BitVec
+  , iotaF200BitVec
   , topEntity
   ) where
 
 import Clash.Prelude
 import Clash.Sized.Internal.BitVector (replaceBit#)
-import SHA3internal (sha3_constants, chi_constants, pi_constants)
+import SHA3internal (sha3_constants, chi_constants, pi_constants, roundConstantsLane)
 
 type State200 = BitVector 200
 
@@ -17,6 +18,10 @@ chiTriples = chi_constants (sha3_constants @3 @8 @200)
 -- Pre-computed constants for Pi permutation
 piIndices :: Vec 200 (Index 200)
 piIndices = pi_constants (sha3_constants @3 @8 @200)
+
+-- Pre-computed round constant for Iota (round 0, w=8 bits)
+iotaRC0 :: Vec 8 Bit
+iotaRC0 = head $ roundConstantsLane @3 @8
 
 -- Chi transformation expressed directly on BitVector
 chiF200BitVec :: BitVector 200 -> BitVector 200
@@ -38,13 +43,24 @@ piF200BitVec bv =
     0
     (zip indicesI piIndices)
 
--- Partial Keccak round: Pi followed by Chi
-piChiF200BitVec :: BitVector 200 -> BitVector 200
-piChiF200BitVec = chiF200BitVec . piF200BitVec
+-- Iota transformation: XOR round constant into first lane (bits 0-7)
+iotaF200BitVec :: BitVector 200 -> BitVector 200
+iotaF200BitVec bv =
+  foldl
+    (\acc (bitIdx, rcBit) ->
+       let oldBit = bv ! fromIntegral bitIdx
+           newBit = oldBit `xor` rcBit
+       in  replaceBit# acc (fromIntegral bitIdx) newBit)
+    bv
+    (zip indicesI iotaRC0)
+
+-- Partial Keccak round: Pi followed by Chi followed by Iota
+piChiIotaF200BitVec :: BitVector 200 -> BitVector 200
+piChiIotaF200BitVec = iotaF200BitVec . chiF200BitVec . piF200BitVec
 
 {-# ANN topEntity
   (Synthesize
-    { t_name = "PiChi_F200BitVec_OneRound"
+    { t_name = "PiChiIota_F200BitVec_OneRound"
     , t_inputs = [ PortName "CLK"
                  , PortName "RST"
                  , PortName "EN"
@@ -59,4 +75,4 @@ topEntity :: Clock System
           -> Enable System
           -> Signal System State200
           -> Signal System State200
-topEntity = exposeClockResetEnable $ fmap piChiF200BitVec
+topEntity = exposeClockResetEnable $ fmap (chiF200BitVec . piF200BitVec)
