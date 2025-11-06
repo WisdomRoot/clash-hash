1228ed0367c5713e034e3762fe7fcaab6e6074fe
Ting-Gian LUA <banacorn@gmail.com>
2025-11-06 15:18:57 +0800

    [ change ] Add `pi` to `ChiF200BitVec` to make `KeccakF200BitVec`

---
+++ src/KeccakF200BitVec.hs
@@ -0,0 +1,62 @@
+module KeccakF200BitVec
+  ( chiF200BitVec
+  , piF200BitVec
+  , topEntity
+  ) where
+
+import Clash.Prelude
+import Clash.Sized.Internal.BitVector (replaceBit#)
+import SHA3internal (sha3_constants, chi_constants, pi_constants)
+
+type State200 = BitVector 200
+
+-- Pre-computed constants for Keccak-f[200]
+chiTriples :: Vec 200 (Index 200, Index 200, Index 200)
+chiTriples = chi_constants (sha3_constants @3 @8 @200)
+
+-- Pre-computed constants for Pi permutation
+piIndices :: Vec 200 (Index 200)
+piIndices = pi_constants (sha3_constants @3 @8 @200)
+
+-- Chi transformation expressed directly on BitVector
+chiF200BitVec :: BitVector 200 -> BitVector 200
+chiF200BitVec bv =
+  foldl
+    (\acc (idx, (i0, i1, i2)) ->
+       let bitOut = bv ! fromIntegral i0 `xor` (complement (bv ! fromIntegral i1) .&. bv ! fromIntegral i2)
+       in  replaceBit# acc (fromIntegral idx) bitOut)
+    0
+    (zip indicesI chiTriples)
+
+-- Pi transformation: bit permutation on BitVector
+piF200BitVec :: BitVector 200 -> BitVector 200
+piF200BitVec bv =
+  foldl
+    (\acc (idx, srcIdx) ->
+       let bitOut = bv ! fromIntegral srcIdx
+       in  replaceBit# acc (fromIntegral idx) bitOut)
+    0
+    (zip indicesI piIndices)
+
+-- Partial Keccak round: Pi followed by Chi
+piChiF200BitVec :: BitVector 200 -> BitVector 200
+piChiF200BitVec = chiF200BitVec . piF200BitVec
+
+{-# ANN topEntity
+  (Synthesize
+    { t_name = "PiChi_F200BitVec_OneRound"
+    , t_inputs = [ PortName "CLK"
+                 , PortName "RST"
+                 , PortName "EN"
+                 , PortName "DIN"
+                 ]
+    , t_output = PortName "DOUT"
+    }) #-}
+{-# OPAQUE topEntity #-}
+
+topEntity :: Clock System
+          -> Reset System
+          -> Enable System
+          -> Signal System State200
+          -> Signal System State200
+topEntity = exposeClockResetEnable $ fmap piChiF200BitVec
