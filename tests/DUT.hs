d7a55881726dd2708e5206c61cabdb7a96645725
Ting-Gian LUA <banacorn@gmail.com>
2025-11-21 17:12:26 +0800

    [ new ] Util for dumping VCD files

--- tests/DUT.hs
+++ tests/DUT.hs
@@ -38,7 +38,7 @@ driveMessage ::
   -> BitVector 64
   -> (Signal System (BitVector 256), Signal System Bool)
 driveMessage dut inputMsg =
-  let (digestSig, doneSig, _, _, _, _, _) = driveMessageDebug dut inputMsg
+  let (digestSig, doneSig, _, _, _, _, _, _, _, _, _) = driveMessageDebug dut inputMsg
    in (digestSig, doneSig)
 
 -- | Debug-friendly harness that exposes handshake and beat counter signals.
@@ -63,8 +63,12 @@ driveMessageDebug ::
      , Signal System (BitVector 64)   -- m_axis_tdata
      , Signal System Bool             -- m_axis_tlast
      , Signal System (Unsigned 3)     -- beat counter
+     , Signal System Bool             -- s_axis_tvalid (stimulus)
+     , Signal System (BitVector 64)   -- s_axis_tdata (stimulus)
+     , Signal System Bool             -- s_axis_tlast (stimulus)
+     , Signal System Bool             -- m_axis_tready (stimulus)
      )
-driveMessageDebug dut inputMsg = (actualDigest, allBeatsCollected, sReady, mValid, mData, mLast, beatNum)
+driveMessageDebug dut inputMsg = (actualDigest, allBeatsCollected, sReady, mValid, mData, mLast, beatNum, sValid, sData, sLast, mReady)
   where
     clk = tbSystemClockGen (not <$> allBeatsCollected)
     rst = systemResetGen
