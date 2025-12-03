f2650828486bc2d3ef853f18892fc3e3c81f2e50
Ting-Gian LUA <banacorn@gmail.com>
2025-12-03 18:56:04 +0800

    [ new ] `partialXOR` for XOR a 64-bit block into state at bit positions [startIdx, startIdx+64)

--- src/Sponge/Stateful5.hs
+++ src/Sponge/Stateful5.hs
@@ -16,6 +16,26 @@ data State
   | Permute (Index 24) (BitVector 1600)
   deriving (Show, Eq, Generic, NFDataX)
 
+-- | XOR a 64-bit block into state at bit positions [startIdx, startIdx+64)
+partialXOR :: BitVector 1600 -> BitVector 64 -> Index 1600 -> BitVector 1600
+partialXOR state block startIdx =
+  let stateVec = unpack state :: Vec 1600 Bit
+      blockVec = unpack block :: Vec 64 Bit
+
+      stateVec' = imap applyXor stateVec
+        where
+          applyXor :: Index 1600 -> Bit -> Bit
+          applyXor i b
+            | i >= startIdx && i < startIdx + 64 =
+                let offset :: Index 1600
+                    offset = i - startIdx
+                    offset64 :: Index 64
+                    offset64 = resize offset
+                 in b `xor` (blockVec !! offset64)
+            | otherwise = b
+
+   in pack stateVec'
+
 -- | Stateful sponge, no streaming interface, fixed 1084-bit input / 256-bit output
 {-# OPAQUE sponge #-}
 sponge ::
@@ -36,6 +56,7 @@ sponge permute = mealy step (Absorb 0)
     step :: State -> BitVector MsgBits -> (State, BitVector digest)
     step (Absorb counter) msg
       | counter < 16 =
+          --
           let absorb :: Vec 1600 Bit -> Vec 1088 Bit -> Vec 1600 Bit
               absorb s block = zipWith xor s (block ++ repeat @512 0)
               blocks = pack (foldl absorb (repeat 0) (Hash.Combinational.pad @MsgBits @n (unpack msg)))
