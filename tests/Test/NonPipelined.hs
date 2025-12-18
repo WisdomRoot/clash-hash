5a91aa5e35bf4e0f7a4684eab9583b1c9830e307
Ting-Gian LUA <banacorn@gmail.com>
2025-12-18 17:14:46 +0800

    [ refactor ] Infrastructure for constructing test cases

--- tests/Test/NonPipelined.hs
+++ tests/Test/NonPipelined.hs
@@ -1,42 +1,16 @@
-{-# LANGUAGE DataKinds #-}
-{-# HLINT ignore "Use head" #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE TypeApplications #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
-
 module Test.NonPipelined (spec) where
 
-import AXI4Stream (AXI4Stream (..))
 import Clash.Prelude hiding (tlast)
 import Data.Foldable (for_)
-import Hash.NonPipelined qualified
 import Reference.SHA3 qualified as SHA3
 import Reference.SHA3internal qualified as SHA3internal
 import Test.Hspec
-import Prelude qualified as P
+import Test.TestCase
 
 spec :: Spec
 spec = describe "NonPipelined SHA3-256 Tests" $ do
-  for_ testCases $ \(TestCase label message expected control) ->
-    it label $ runCase message expected control
-
---
-
-data SomeMessage where
-  SomeMessage :: KnownNat beats => Vec (beats * 64) Bit -> SomeMessage
-
-data TestCase = TestCase
-  { npLabel :: String,
-    npMessage :: SomeMessage,
-    npExpected :: Vec 4 (BitVector 64),
-    npInputControl :: NPInputControl
-  }
-
-data NPInputControl
-  = NoUpstreamStall
-  | UpstreamStall [Bool]
+  for_ testCases $ \testCase ->
+    it (testCaseLabel testCase) $ run testCase
 
 testCases :: [TestCase]
 testCases =
@@ -96,82 +70,3 @@ testCases =
         True,
         True
       ]
-
-runCase ::
-  SomeMessage ->
-  Vec 4 (BitVector 64) ->
-  NPInputControl ->
-  Expectation
-runCase (SomeMessage (message :: Vec (beats * 64) Bit)) expected control = do
-  let messageWords = bitCoerce message :: Vec beats (BitVector 64)
-      inputStream =
-        withClockResetEnable clockGen resetGen enableGen
-          $ runInputStream control messageWords
-      output =
-        Hash.NonPipelined.topEntity
-          clockGen
-          resetGen
-          enableGen
-          (pure True)
-          inputStream
-      beatCount = natToNum @beats :: Int
-      blockTransitions =
-        if beatCount <= 0
-          then 0
-          else P.max 0 (beatCount - 1) `div` 17
-      gapCycles = blockTransitions * 24
-      permutationCount = blockTransitions + 1
-      sampleCount = beatCount + gapCycles + 1 + permutationCount * 24 + 4 + 64
-      samples = sampleN @System sampleCount output
-      actualStreams = P.take 4 $ P.filter (tvalid . fst) samples
-
-  fmap (tdata . fst) actualStreams `shouldBe` toList expected
-  fmap (tvalid . fst) actualStreams `shouldBe` P.replicate 4 True
-  fmap (tlast . fst) actualStreams `shouldBe` [False, False, False, True]
-
-runInputStream ::
-  forall beats dom.
-  ( KnownNat beats,
-    HiddenClockResetEnable dom
-  ) =>
-  NPInputControl ->
-  Vec beats (BitVector 64) ->
-  Signal dom (AXI4Stream 64)
-runInputStream control messageWords =
-  mealy step (toList messageWords, 0 :: Int, 0 :: Int, controlToList control) (pure ())
-  where
-    controlToList NoUpstreamStall = []
-    controlToList (UpstreamStall xs) = xs
-
-    step (xs, waitCount, emittedInBlock, ctrl) _ =
-      let (canSend, ctrl') =
-            case ctrl of
-              [] -> (True, [])
-              b : bs -> (b, bs)
-       in if waitCount > 0
-            then ((xs, waitCount - 1, emittedInBlock, ctrl'), idleBeat)
-            else
-              if not canSend
-                then ((xs, waitCount, emittedInBlock, ctrl'), idleBeat)
-                else case xs of
-                  [] -> (([], 0, 0, ctrl'), idleBeat)
-                  y : ys ->
-                    let isLast = P.null ys
-                        emittedNow = emittedInBlock + 1
-                        needGap = emittedNow == 17 && not isLast
-                        nextWait = if needGap then 24 else 0
-                        nextEmitted = if needGap then 0 else emittedNow
-                        nextState = (ys, nextWait, nextEmitted, ctrl')
-                        outBeat =
-                          AXI4Stream
-                            { tdata = y,
-                              tvalid = True,
-                              tlast = isLast
-                            }
-                     in (nextState, outBeat)
-    idleBeat =
-      AXI4Stream
-        { tdata = 0,
-          tvalid = False,
-          tlast = False
-        }
