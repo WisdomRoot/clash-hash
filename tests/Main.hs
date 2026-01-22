6e410525f25e109099677bc36d8922bdcf4895ef
Ting-Gian LUA <banacorn@gmail.com>
2026-01-22 11:33:59 +0800

    [ new ] Parallel SHA3-256 for later index reversal

--- tests/Main.hs
+++ tests/Main.hs
@@ -6,6 +6,7 @@ module Main (main) where
 import Test.Combinational qualified
 import Test.Constants qualified
 import Test.NonPipelined.SHA3256 qualified
+import Test.NonPipelined.SHA3256Reversed qualified
 import Test.NonPipelined.SHAKE256 qualified
 import Test.NonPipelined.SHAKE128 qualified
 import Test.NonPipelined.SHAKE128B qualified
@@ -26,6 +27,7 @@ main = do
   permutationRevTests <- testSpec "Permutation.Reversed" Test.Permutation.Reversed.spec
   combinationalTests <- testSpec "Combinational" Test.Combinational.spec
   n256Tests <- testSpec "NonPipelined SHA3-256" Test.NonPipelined.SHA3256.spec
+  n256RevTests <- testSpec "NonPipelined SHA3-256 Reversed" Test.NonPipelined.SHA3256Reversed.spec
   n256xTests <- testSpec "NonPipelined SHAKE-256" Test.NonPipelined.SHAKE256.spec
   n128xTests <- testSpec "NonPipelined SHAKE-128" Test.NonPipelined.SHAKE128.spec
   n128xbTests <- testSpec "NonPipelined SHAKE-128B" Test.NonPipelined.SHAKE128B.spec
@@ -44,6 +46,7 @@ main = do
         permutationRevTests,
         combinationalTests,
         n256Tests,
+        n256RevTests,
         n256xTests,
         n128xTests,
         n128xbTests,
