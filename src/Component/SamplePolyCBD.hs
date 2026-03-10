6255d814886b6ca6ff3ff62e67d1a24a40dad2a5
Ting-Gian LUA <banacorn@gmail.com>
2026-03-10 15:42:08 +0800

    [ new ] CBD-O24

--- src/Component/SamplePolyCBD.hs
+++ src/Component/SamplePolyCBD.hs
@@ -3,7 +3,8 @@
 {-# LANGUAGE TypeOperators #-}
 
 module Component.SamplePolyCBD
-  ( i272o12
+  ( i272o12,
+    i272o24
   )
 where
 
@@ -37,6 +38,20 @@ data State
   | Eta3Permute (Index 24) (Index 256) Eta3Phase (BitVector 1600)
   | Eta3Squeeze (Index 256) Eta3Phase (BitVector 1600)
   | Done
+  deriving (Show, Eq, Generic, NFDataX)
+
+data Eta3PairPhase
+  = Eta3PairFirstBlock
+  | Eta3PairSecondBlock (BitVector 8)
+  deriving (Show, Eq, Generic, NFDataX)
+
+data State24
+  = Absorb24
+  | Eta2Permute24 (Index 24) (Index 128) (BitVector 1600)
+  | Eta2Squeeze24 (Index 128) (BitVector 1600)
+  | Eta3Permute24 (Index 24) (Index 128) Eta3PairPhase (BitVector 1600)
+  | Eta3Squeeze24 (Index 128) Eta3PairPhase (BitVector 1600)
+  | Done24
   deriving (Show, Eq, Generic, NFDataX)
 
 isEta3 :: BitVector 8 -> Bool
@@ -144,6 +159,101 @@ i272o12Core ::
 i272o12Core (outReady, inStream) =
   mealyB stepI272o12 Absorb (outReady, inStream)
 
+i272o24Core ::
+  HiddenClockResetEnable dom =>
+  Pipe dom 272 24
+i272o24Core (outReady, inStream) =
+  mealyB stepI272o24 Absorb24 (outReady, inStream)
+
+stepI272o24 ::
+  State24 ->
+  (Bool, AXI4Stream 272) ->
+  (State24, (Bool, AXI4Stream 24))
+stepI272o24 st (outReady, inStream) =
+  case st of
+    Absorb24 ->
+      if tvalid inStream
+        then
+          let msg272 = tdata inStream
+              etaByte = slice (SNat @271) (SNat @264) msg272
+              msg264 = slice (SNat @263) (SNat @0) msg272
+           in if isEta3 etaByte
+                then
+                  let initState = absorb33Normal msg264
+                   in (Eta3Permute24 0 0 Eta3PairFirstBlock initState, (False, idleAXI4Stream))
+                else
+                  let initState = absorb33Normal msg264
+                   in (Eta2Permute24 0 0 initState, (False, idleAXI4Stream))
+        else (Absorb24, (True, idleAXI4Stream))
+    Eta2Permute24 roundIdx pairIdx state ->
+      let state' = Permutation.keccakF1600 roundIdx state
+       in if roundIdx == maxBound
+            then (Eta2Squeeze24 pairIdx state', (False, idleAXI4Stream))
+            else (Eta2Permute24 (roundIdx + 1) pairIdx state', (False, idleAXI4Stream))
+    Eta2Squeeze24 pairIdx state ->
+      let pairIdxU = fromIntegral pairIdx :: Unsigned 8
+          idx0 = fromIntegral (pairIdxU * 2) :: Index 256
+          idx1 = fromIntegral (pairIdxU * 2 + 1) :: Index 256
+          coeff0 = cbd2Ref (read4Block0 state idx0)
+          coeff1 = cbd2Ref (read4Block0 state idx1)
+          isLast = pairIdx == maxBound
+          outStream = validBeat (coeff1 ++# coeff0) isLast
+          nextState
+            | outReady && isLast = Done24
+            | outReady = Eta2Squeeze24 (pairIdx + 1) state
+            | otherwise = Eta2Squeeze24 pairIdx state
+       in (nextState, (False, outStream))
+    Eta3Permute24 roundIdx pairIdx phase state ->
+      let state' = Permutation.keccakF1600 roundIdx state
+       in if roundIdx == maxBound
+            then (Eta3Squeeze24 pairIdx phase state', (False, idleAXI4Stream))
+            else (Eta3Permute24 (roundIdx + 1) pairIdx phase state', (False, idleAXI4Stream))
+    Eta3Squeeze24 pairIdx phase block ->
+      case phase of
+        Eta3PairFirstBlock ->
+          if pairIdx >= 90
+            then
+              let bits8 = slice (SNat @1087) (SNat @1080) block
+               in (Eta3Permute24 0 pairIdx (Eta3PairSecondBlock bits8) block, (False, idleAXI4Stream))
+            else
+              let pairIdxU = fromIntegral pairIdx :: Unsigned 8
+                  idx0 = fromIntegral (pairIdxU * 2) :: Index 181
+                  idx1 = fromIntegral (pairIdxU * 2 + 1) :: Index 181
+                  coeff0 = cbd3 (read6Block0 block idx0)
+                  coeff1 = cbd3 (read6Block0 block idx1)
+                  outStream = validBeat (coeff1 ++# coeff0) False
+                  nextState
+                    | outReady = Eta3Squeeze24 (pairIdx + 1) Eta3PairFirstBlock block
+                    | otherwise = Eta3Squeeze24 pairIdx Eta3PairFirstBlock block
+               in (nextState, (False, outStream))
+        Eta3PairSecondBlock bits8 ->
+          let (outData, isLast) =
+                if pairIdx == 90
+                  then
+                    let tail2 = slice d7 d6 bits8
+                        coeff180Bits = slice d5 d0 bits8
+                        coeff180 = cbd3 coeff180Bits
+                        head4 = slice d3 d0 block
+                        coeff181 = cbd3 (head4 ++# tail2)
+                     in (coeff181 ++# coeff180, False)
+                  else
+                    let pairIdxU = fromIntegral pairIdx :: Unsigned 8
+                        coeff0U = pairIdxU * 2
+                        coeff1U = coeff0U + 1
+                        idx0 = fromIntegral (coeff0U - 182) :: Index 74
+                        idx1 = fromIntegral (coeff1U - 182) :: Index 74
+                        coeff0 = cbd3 (read6Block1 block idx0)
+                        coeff1 = cbd3 (read6Block1 block idx1)
+                        lastPair = pairIdx == maxBound
+                     in (coeff1 ++# coeff0, lastPair)
+              outStream = validBeat outData isLast
+              nextState
+                | outReady && isLast = Done24
+                | outReady = Eta3Squeeze24 (pairIdx + 1) (Eta3PairSecondBlock bits8) block
+                | otherwise = Eta3Squeeze24 pairIdx (Eta3PairSecondBlock bits8) block
+           in (nextState, (False, outStream))
+    Done24 -> (Done24, (False, idleAXI4Stream))
+
 {-# ANN
   i272o12
   ( Synthesize
@@ -175,3 +285,35 @@ i272o12 ::
   Signal System (AXI4Stream 272, Bool) ->
   Signal System (AXI4Stream 12, Bool)
 i272o12 = toDUT i272o12Core
+
+{-# ANN
+  i272o24
+  ( Synthesize
+      { t_name = "dut",
+        t_inputs =
+          [ PortName "CLK",
+            PortName "RST",
+            PortName "EN",
+            PortProduct
+              ""
+              [ PortProduct "MSG" [PortName "TDATA", PortName "TVALID", PortName "TLAST"],
+                PortName "COEFF_TREADY"
+              ]
+          ],
+        t_output =
+          PortProduct
+            ""
+            [ PortProduct "COEFF" [PortName "TDATA", PortName "TVALID", PortName "TLAST"],
+              PortName "MSG_TREADY"
+            ]
+      }
+  )
+  #-}
+{-# NOINLINE i272o24 #-}
+i272o24 ::
+  Clock System ->
+  Reset System ->
+  Enable System ->
+  Signal System (AXI4Stream 272, Bool) ->
+  Signal System (AXI4Stream 24, Bool)
+i272o24 = toDUT i272o24Core
--- tests/Test/SamplePolyCBD.hs
+++ tests/Test/SamplePolyCBD.hs
@@ -5,7 +5,7 @@
 module Test.SamplePolyCBD (spec) where
 
 import AXI4Stream (Pipe)
-import Clash.Prelude (BitVector, SNat (..), System, bundle, clockGen, enableGen, resetGen, slice, unbundle)
+import Clash.Prelude (BitVector, SNat (..), System, (++#), bundle, clockGen, enableGen, resetGen, slice, unbundle)
 import Component.PRF.Common (Eta (Eta2, Eta3))
 import Component.SamplePolyCBD qualified as SamplePolyCBD
 import Data.ByteString qualified as BS
@@ -23,6 +23,12 @@ i272o12AsPipe :: Pipe System 272 12
 i272o12AsPipe (outReady, inStream) =
   let (outStream, inReady) =
         unbundle (SamplePolyCBD.i272o12 clockGen resetGen enableGen (bundle (inStream, outReady)))
+   in (inReady, outStream)
+
+i272o24AsPipe :: Pipe System 272 24
+i272o24AsPipe (outReady, inStream) =
+  let (outStream, inReady) =
+        unbundle (SamplePolyCBD.i272o24 clockGen resetGen enableGen (bundle (inStream, outReady)))
    in (inReady, outStream)
 
 etaFromInput :: BitVector 272 -> Eta
@@ -54,6 +60,33 @@ simulate inputTiming =
             let (c0, c1) = P.splitAt 181 coeffs
              in [Silent 25, Output c0, Silent 25, Output c1]
    in if startSilence P.== 0 then base else Silent startSilence : base
+
+simulate24 :: InputTiming 272 -> OutputTiming 24
+simulate24 inputTiming =
+  let (inputPattern, inputValues) = expandInputTiming inputTiming
+      inputBV =
+        case inputValues of
+          (v : _) -> v
+          [] -> P.error "Test.SamplePolyCBD.simulate24: no input provided"
+      startSilence =
+        case L.findIndex isJust inputPattern of
+          Just i -> i
+          Nothing -> P.error "Test.SamplePolyCBD.simulate24: no input provided"
+      eta = etaFromInput inputBV
+      msg264 = msg264FromInput inputBV
+      coeffs = Reference.run eta msg264
+      pairs = pairCoeffs coeffs
+      base =
+        case eta of
+          Eta2 -> [Silent 25, Output pairs]
+          Eta3 ->
+            let (p0, p1) = P.splitAt 90 pairs
+             in [Silent 25, Output p0, Silent 25, Output p1]
+   in if startSilence P.== 0 then base else Silent startSilence : base
+  where
+    pairCoeffs (c0 : c1 : rest) = (c1 ++# c0) : pairCoeffs rest
+    pairCoeffs [c0] = [(0 :: BitVector 12) ++# c0]
+    pairCoeffs [] = []
 
 compressBackpressure :: [P.Bool] -> BackpressureTiming
 compressBackpressure [] = []
@@ -100,7 +133,17 @@ spec = describe "SamplePolyCBD (Stream)" $ do
   it "matches expected output (eta=3, periodic backpressure)" $ do
     let input = toBV @272 ("0123456789abcdef0123456789abcdef!" P.<> BS.pack [3])
     runPipeInput i272o12AsPipe simulate [Input [input]] [Ready 2, Backpress 1]
+  it "i272o24 matches expected output (eta=2, no backpressure)" $ do
+    let input = toBV @272 ("0123456789abcdef0123456789abcdef!" P.<> BS.pack [2])
+    runPipeInput i272o24AsPipe simulate24 [Input [input]] [Ready 1]
+  it "i272o24 matches expected output (eta=3, no backpressure)" $ do
+    let input = toBV @272 ("0123456789abcdef0123456789abcdef!" P.<> BS.pack [3])
+    runPipeInput i272o24AsPipe simulate24 [Input [input]] [Ready 1]
   describe "QuickCheck property tests" $
     it "matches reference for random inputs and backpressure" $
       forAll genCase $ \(inputTiming, backpressure) ->
         runPipeInput i272o12AsPipe simulate inputTiming backpressure
+  describe "QuickCheck property tests (i272o24)" $
+    it "matches reference for random inputs and backpressure" $
+      forAll genCase $ \(inputTiming, backpressure) ->
+        runPipeInput i272o24AsPipe simulate24 inputTiming backpressure
