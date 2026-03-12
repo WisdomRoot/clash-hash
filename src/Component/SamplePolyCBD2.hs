2da1a3a469f0cda8438f5a0fda217897fea407e8
Ting-Gian LUA <banacorn@gmail.com>
2026-03-12 15:22:21 +0800

    [ new ] Introduce CBD2-O24

--- src/Component/SamplePolyCBD2.hs
+++ src/Component/SamplePolyCBD2.hs
@@ -1,6 +1,8 @@
 module Component.SamplePolyCBD2
   ( i264o12,
     i264o12Core,
+    i264o24,
+    i264o24Core,
   )
 where
 
@@ -46,6 +48,38 @@ i264o12 ::
   Signal System (AXI4Stream 12, Bool)
 i264o12 = toDUT i264o12Core
 
+{-# ANN
+  i264o24
+  ( Synthesize
+      { t_name = "dut",
+        t_inputs =
+          [ PortName "CLK",
+            PortName "RST",
+            PortName "EN",
+            PortProduct
+              ""
+              [ PortProduct "MSG" [PortName "TDATA", PortName "TVALID", PortName "TLAST"],
+                PortName "DIGEST_TREADY"
+              ]
+          ],
+        t_output =
+          PortProduct
+            ""
+            [ PortProduct "DIGEST" [PortName "TDATA", PortName "TVALID", PortName "TLAST"],
+              PortName "MSG_TREADY"
+            ]
+      }
+  )
+  #-}
+{-# NOINLINE i264o24 #-}
+i264o24 ::
+  Clock System ->
+  Reset System ->
+  Enable System ->
+  Signal System (AXI4Stream 264, Bool) ->
+  Signal System (AXI4Stream 24, Bool)
+i264o24 = toDUT i264o24Core
+
 -- | State machine for SamplePolyCBD2
 -- PRF(eta=2) outputs 128 bytes = 16 64-bit words
 -- Each 64-bit word produces 16 coefficients via CBD(eta=2)
@@ -55,6 +89,12 @@ data State
   | Permute (Index 24) (BitVector 1600)
   | Squeeze (Index 16) (Index 16) (BitVector 1600) -- wordIdx, coeffIdx within word, state
   | Done
+  deriving (Show, Eq, Generic, NFDataX)
+
+data PairState
+  = NeedFirst
+  | NeedSecond (BitVector 12)
+  | HavePair (BitVector 24) Bool
   deriving (Show, Eq, Generic, NFDataX)
 
 samplePolyCBD2 ::
@@ -112,3 +152,33 @@ i264o12Core ::
   HiddenClockResetEnable dom =>
   Pipe dom 264 12
 i264o12Core = samplePolyCBD2
+
+pairStep ::
+  PairState ->
+  (Bool, AXI4Stream 12) ->
+  (PairState, (Bool, AXI4Stream 24))
+pairStep st (outReady, inBeat) =
+  case st of
+    NeedFirst ->
+      if tvalid inBeat
+        then (NeedSecond (tdata inBeat), (True, idleAXI4Stream))
+        else (NeedFirst, (True, idleAXI4Stream))
+    NeedSecond c0 ->
+      if tvalid inBeat
+        then
+          let pairData = tdata inBeat ++# c0
+              outBeat = validBeat pairData (tlast inBeat)
+           in if outReady
+                then (NeedFirst, (True, outBeat))
+                else (HavePair pairData (tlast inBeat), (True, outBeat))
+        else (NeedSecond c0, (True, idleAXI4Stream))
+    HavePair pairData isLast ->
+      let outBeat = validBeat pairData isLast
+       in if outReady
+            then (NeedFirst, (False, outBeat))
+            else (HavePair pairData isLast, (False, outBeat))
+
+i264o24Core ::
+  HiddenClockResetEnable dom =>
+  Pipe dom 264 24
+i264o24Core = i264o12Core ~> mealyB pairStep NeedFirst
