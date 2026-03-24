e6be5be66760019f7e3efac6dc03e611ff16f840
Ting-Gian LUA <banacorn@gmail.com>
2026-03-24 13:15:07 +0800

    [ new ] Make CBD2-O24-dev smaller by reducing the core algorithm to a 3x3 table

--- src/Component/SamplePolyCBD2Dev.hs
+++ src/Component/SamplePolyCBD2Dev.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Component.SamplePolyCBD2Dev
   ( i264o12,
     i264o12Core,
@@ -8,7 +12,132 @@ where
 
 import AXI4Stream
 import Clash.Prelude hiding (tlast)
-import Component.SamplePolyCBD qualified as SamplePolyCBD
+import Permutation qualified
+import Sponge.NonPipelined (complementAt)
+import TH (mkRead)
+
+-- | Extract 4-bit chunks for eta=2 path.
+$(mkRead "read4Block0" 1600 [(i, i * 4, 4) | i <- [0 .. 255]])
+
+data State
+  = Absorb
+  | Eta2Permute (Index 24) (Index 256) (BitVector 1600)
+  | Eta2Squeeze (Index 256) (BitVector 1600)
+  | Done
+  deriving (Show, Eq, Generic, NFDataX)
+
+data State24
+  = Absorb24
+  | Eta2Permute24 (Index 24) (Index 128) (BitVector 1600)
+  | Eta2Squeeze24 (Index 128) (BitVector 1600)
+  | Done24
+  deriving (Show, Eq, Generic, NFDataX)
+
+cbd2Table :: BitVector 4 -> BitVector 12
+cbd2Table bits =
+  let x = pairSum (slice d1 d0 bits)
+      y = pairSum (slice d3 d2 bits)
+   in case (x, y) of
+        (0, 0) -> 0
+        (0, 1) -> 3328
+        (0, 2) -> 3327
+        (1, 0) -> 1
+        (1, 1) -> 0
+        (1, 2) -> 3328
+        (2, 0) -> 2
+        (2, 1) -> 1
+        (2, 2) -> 0
+        _ -> errorX "Component.SamplePolyCBD2Dev.cbd2Table: impossible"
+  where
+    pairSum :: BitVector 2 -> Unsigned 2
+    pairSum pairBits =
+      resize (unpack (slice d0 d0 pairBits) :: Unsigned 1)
+        + resize (unpack (slice d1 d1 pairBits) :: Unsigned 1)
+
+absorb33Normal :: BitVector 264 -> BitVector 1600
+absorb33Normal = pad33Bytes . placeMsg
+  where
+    placeMsg msg = (0 :: BitVector 1336) ++# msg
+    pad33Bytes =
+      complementAt 1087
+        . complementAt 264
+        . complementAt 265
+        . complementAt 266
+        . complementAt 267
+        . complementAt 268
+
+stepI264o12 ::
+  State ->
+  (Bool, AXI4Stream 264) ->
+  (State, (Bool, AXI4Stream 12))
+stepI264o12 st (outReady, inStream) =
+  case st of
+    Absorb ->
+      if tvalid inStream
+        then
+          let initState = absorb33Normal (tdata inStream)
+           in (Eta2Permute 0 0 initState, (False, idleAXI4Stream))
+        else (Absorb, (True, idleAXI4Stream))
+    Eta2Permute roundIdx coeffIdx state ->
+      let state' = Permutation.keccakF1600 roundIdx state
+       in if roundIdx == maxBound
+            then (Eta2Squeeze coeffIdx state', (False, idleAXI4Stream))
+            else (Eta2Permute (roundIdx + 1) coeffIdx state', (False, idleAXI4Stream))
+    Eta2Squeeze coeffIdx state ->
+      let bits4 = read4Block0 state coeffIdx
+          coeff = cbd2Table bits4
+          isLast = coeffIdx == maxBound
+          outStream = validBeat coeff isLast
+          nextState
+            | outReady && isLast = Done
+            | outReady = Eta2Squeeze (coeffIdx + 1) state
+            | otherwise = Eta2Squeeze coeffIdx state
+       in (nextState, (False, outStream))
+    Done -> (Done, (False, idleAXI4Stream))
+
+stepI264o24 ::
+  State24 ->
+  (Bool, AXI4Stream 264) ->
+  (State24, (Bool, AXI4Stream 24))
+stepI264o24 st (outReady, inStream) =
+  case st of
+    Absorb24 ->
+      if tvalid inStream
+        then
+          let initState = absorb33Normal (tdata inStream)
+           in (Eta2Permute24 0 0 initState, (False, idleAXI4Stream))
+        else (Absorb24, (True, idleAXI4Stream))
+    Eta2Permute24 roundIdx pairIdx state ->
+      let state' = Permutation.keccakF1600 roundIdx state
+       in if roundIdx == maxBound
+            then (Eta2Squeeze24 pairIdx state', (False, idleAXI4Stream))
+            else (Eta2Permute24 (roundIdx + 1) pairIdx state', (False, idleAXI4Stream))
+    Eta2Squeeze24 pairIdx state ->
+      let pairIdxU = fromIntegral pairIdx :: Unsigned 8
+          idx0 = fromIntegral (pairIdxU * 2) :: Index 256
+          idx1 = fromIntegral (pairIdxU * 2 + 1) :: Index 256
+          coeff0 = cbd2Table (read4Block0 state idx0)
+          coeff1 = cbd2Table (read4Block0 state idx1)
+          isLast = pairIdx == maxBound
+          outStream = validBeat (coeff1 ++# coeff0) isLast
+          nextState
+            | outReady && isLast = Done24
+            | outReady = Eta2Squeeze24 (pairIdx + 1) state
+            | otherwise = Eta2Squeeze24 pairIdx state
+       in (nextState, (False, outStream))
+    Done24 -> (Done24, (False, idleAXI4Stream))
+
+i264o12Core ::
+  HiddenClockResetEnable dom =>
+  Pipe dom 264 12
+i264o12Core (outReady, inStream) =
+  mealyB stepI264o12 Absorb (outReady, inStream)
+
+i264o24Core ::
+  HiddenClockResetEnable dom =>
+  Pipe dom 264 24
+i264o24Core (outReady, inStream) =
+  mealyB stepI264o24 Absorb24 (outReady, inStream)
 
 {-# ANN
   i264o12
@@ -73,24 +202,3 @@ i264o24 ::
   Signal System (AXI4Stream 264, Bool) ->
   Signal System (AXI4Stream 24, Bool)
 i264o24 = toDUT i264o24Core
-
-prependEta ::
-  HiddenClockResetEnable dom =>
-  BitVector 8 ->
-  Pipe dom 264 272
-prependEta eta (outReady, inStream) =
-  let outStream = fmap prepend inStream
-   in (outReady, outStream)
-  where
-    prepend (AXI4Stream msg valid lastBeat) =
-      AXI4Stream (eta ++# msg) valid lastBeat
-
-i264o12Core ::
-  HiddenClockResetEnable dom =>
-  Pipe dom 264 12
-i264o12Core = prependEta 2 ~> SamplePolyCBD.i272o12Core
-
-i264o24Core ::
-  HiddenClockResetEnable dom =>
-  Pipe dom 264 24
-i264o24Core = prependEta 2 ~> SamplePolyCBD.i272o24Core
