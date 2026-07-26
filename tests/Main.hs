4a9435b265363f1566646b1f8545b7e3b5077216
謝杰宇 <jasonjj0103@gmail.com>
2026-07-26 12:35:54 +0800

    Main cabal

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,10 +1,7 @@
 module Main (main) where
 
-import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.Hspec (testSpec)
+import Test.Hspec (hspec)
 import Test.NTT qualified as NTT
 
 main :: IO ()
-main = do
-  nttTests <- testSpec "NTT" NTT.spec
-  defaultMain (testGroup "All Tests" [nttTests])
+main = hspec NTT.spec
